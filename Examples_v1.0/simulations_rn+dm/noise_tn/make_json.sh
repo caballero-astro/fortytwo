21eedb88d5f446e1640edd1cf496ce0e225bfd70
R.N. Caballero <132844619+caballero-astro@users.noreply.github.com>
2025-01-15 20:10:21 +0200

    Deleted junk file
    Signed-off-by: R.N. Caballero <132844619+caballero-astro@users.noreply.github.com>

--- Examples_v1.0/simulations_rn+dm/noise_tn/make_json.sh
+++
@@ -1,5 +0,0 @@
-for psr in $(cat ../../sims_epta+inpta_radiometer-wn/25PSR_list.txt); do
-	echo { > ${psr}_noise2.json
-	grep ${psr} crn_noise2.json >> ${psr}_noise2.json
-	echo } >> ${psr}_noise2.json
-done	
